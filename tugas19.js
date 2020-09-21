edfc4d6307b3a4b0054984e803ef18fc471520f7
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 11:42:50 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+var kalimat1 = "Semangat..."
+var hasil1 = kalimat1.repeat(5)
+
+var kalimat2 = "*"
+var hasil2 = kalimat2.repeat(3)
+var hasil3 = kalimat2.repeat(6)
+var hasil4 = kalimat2.repeat(12)
+var hasil5 = kalimat2.repeat (15)
+var hasil6 = kalimat2.repeat(12)
+var hasil7 = kalimat2.repeat(6)
+var hasil8 = kalimat2.repeat(3)
+
+console.log (hasil1)
+console.log	(hasil2)
+console.log	(hasil3)
+console.log	(hasil4)
+console.log	(hasil5)
+console.log	(hasil6)
+console.log	(hasil7)
+console.log	(hasil8)
